5333d634a53085546c7ead8b7a00b946c306f798
Ulises Taveras <7twily@gmail.com>
2018-04-13 01:09:01 -0400

    Created UI and task adding logic - New item binds to added item (bug)

--- app/js/controllers/maincontroller.js
+++ app/js/controllers/maincontroller.js
@@ -2,7 +2,18 @@
     const app = angular.module('angularAgenda');
 
     function mainController($scope) {
-        $scope.message = 'I works';
+        $scope.agenda = [
+            {
+                "description": "Testing",
+                "time": "1970-01-01T17:00:00.000Z"
+            }
+        ];
+        $scope.newTask = {};
+
+        $scope.addTask = function addTask() {
+            const item = $scope.newTask;
+            $scope.agenda.push(item);
+        }
     }
 
     app.controller('mainController', mainController);
