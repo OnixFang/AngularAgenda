cf12c8a70350d4f15e7309e6a1ef6836045929cc
Ulises Taveras <7twily@gmail.com>
2018-05-15 00:25:37 -0400

    Add Task constructor to fix bind bug

--- app/js/controllers/maincontroller.js
+++ app/js/controllers/maincontroller.js
@@ -2,17 +2,20 @@
     const app = angular.module('angularAgenda');
 
     function mainController($scope) {
-        $scope.agenda = [
-            {
-                "description": "Testing",
-                "time": "1970-01-01T17:00:00.000Z"
-            }
-        ];
-        $scope.newTask = {};
+        $scope.agenda = [];
 
-        $scope.addTask = function addTask() {
-            const item = $scope.newTask;
-            $scope.agenda.push(item);
+        function task(description, time) {
+            this.description = description;
+            this.time = time;
+        }
+
+        $scope.addTask = function addTask(newTask) {
+            const taskObj = new task(newTask.description, newTask.time);
+            $scope.agenda.push(taskObj);
+        }
+
+        $scope.test = function test() {
+            console.log($scope.agenda);
         }
     }
 
